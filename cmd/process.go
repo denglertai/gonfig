ac899c34a038accd87bfc88b0a6dd053303f6424
Daniel Englert <danield.englert@gmx.de>
2024-11-23 17:54:55 +0100

    cmd structure

---
+++ cmd/process.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// processCmd represents the process command
+var processCmd = &cobra.Command{
+	Use:   "process",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("process called")
+	},
+}
+
+func init() {
+	configCmd.AddCommand(processCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// processCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// processCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
